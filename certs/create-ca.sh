fa14fda7f164278b0a4e34b9028e200f38e97dec
sjsullivan7 <sjsullivan7@gmail.com>
2019-11-14 23:03:40 -0500

    Update to set password on pks12 but nifi is still having an issue

--- certs/create-ca.sh
+++ certs/create-ca.sh
@@ -2,7 +2,7 @@
 
 # This is just for testing and development
 # For a real use case you would want to set a password on the ca (don't use the -nodes option)
-openssl req -x509 -config openssl-ca.cnf -newkey rsa:4096 -sha256 -nodes -out ca_crt.pem -outform PEM -batch
+openssl req -x509 -config openssl-ca.cnf -newkey rsa:4096 -sha256 -passout pass:changeit -out ca_crt.pem -outform PEM -batch
 openssl pkcs12 -export -out ca.p12 -inkey ca_key.pem -in ca_crt.pem -passout pass:changeit
 touch index.txt
 echo '01' > serial.txt
--- certs/create-server.sh
+++ certs/create-server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-openssl req -config openssl-server.cnf -newkey rsa:4096 -sha256 -nodes -out server.csr -outform PEM -batch
+openssl req -config openssl-server.cnf -newkey rsa:4096 -passout pass:changeit  -sha256 -out server.csr -outform PEM -batch
 openssl ca -batch -config openssl-ca.cnf -policy signing_policy -extensions signing_req -out server_crt.pem -infiles server.csr
-openssl pkcs12 -export -out server.p12 -inkey server_key.pem -in server_crt.pem -passout pass:chaangeit
+openssl pkcs12 -export -out server.p12 -inkey server_key.pem -in server_crt.pem -passout pass:changeit
