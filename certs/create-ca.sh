75506919b30715486e81ade68af3be7920be6fa7
sjsullivan7 <sjsullivan7@gmail.com>
2019-11-14 23:12:35 -0500

    Set in and out pass

--- certs/create-ca.sh
+++ certs/create-ca.sh
@@ -3,6 +3,7 @@
 # This is just for testing and development
 # For a real use case you would want to set a password on the ca (don't use the -nodes option)
 openssl req -x509 -config openssl-ca.cnf -newkey rsa:4096 -sha256 -passout pass:changeit -out ca_crt.pem -outform PEM -batch
-openssl pkcs12 -export -out ca.p12 -inkey ca_key.pem -in ca_crt.pem -passout pass:changeit
+openssl pkcs12 -export -out ca.p12 -inkey ca_key.pem -in ca_crt.pem -passin pass:changeit -passout pass:changeit
+mkdir -p signed
 touch index.txt
 echo '01' > serial.txt
--- certs/create-server.sh
+++ certs/create-server.sh
@@ -2,4 +2,4 @@
 
 openssl req -config openssl-server.cnf -newkey rsa:4096 -passout pass:changeit  -sha256 -out server.csr -outform PEM -batch
 openssl ca -batch -config openssl-ca.cnf -policy signing_policy -extensions signing_req -out server_crt.pem -infiles server.csr
-openssl pkcs12 -export -out server.p12 -inkey server_key.pem -in server_crt.pem -passout pass:changeit
+openssl pkcs12 -batch -export -out server.p12 -inkey server_key.pem -in server_crt.pem -passin pass:changeit -passout pass:changeit
